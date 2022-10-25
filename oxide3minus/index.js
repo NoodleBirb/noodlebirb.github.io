35feaaab907500fc76f055fdfd9b2d12da007e7f
NoodleBirb <62676072+NoodleBirb@users.noreply.github.com>
2022-10-25 12:03:09 -0400

    Update index.js

--- oxide3minus/index.js
+++ oxide3minus/index.js
@@ -30,7 +30,6 @@ function checkAnswer() {
         textOfSomeSort.innerText = "you got it wrong, it should've been " + elements[ranIndex][1];
         incor += 1;
         total += 1;
-        setTimeout(changeElement, 1000);
         
         ans.value = "";
     }
